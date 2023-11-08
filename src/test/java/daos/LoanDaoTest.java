e883d2e7e5e25012a0ea3f5448e652e4ab74cf86
Tom <tomzhou666@outlook.com>
2023-11-08 10:08:56 +0000

    putting code in methods in app

---
+++ src/test/java/daos/LoanDaoTest.java
@@ -0,0 +1,21 @@
+package daos;
+
+import business.Loan;
+import daos.LoanDao;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Tom
+ */
+
+public class LoanDaoTest {
+    public LoanDaoTest() {
+
+    }
+
+    public void testGetCurrentLoan(int userId) {
+
+    }
+}
